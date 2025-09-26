64e781c52e1a4cd9160c7b8cae0862aeb0103dc9
Buvanesh <buvanesh.ganeshan@redingtongroup.com>
2025-09-26 10:10:04 +0530

    service

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "dev-aman-tf-bucket-2" #serves
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    #dynamodb_table = "Lock-Files"
     encrypt        = true
   }
 }
